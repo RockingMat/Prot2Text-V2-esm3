9b07d871c2ba6544d9b1acad79439a7eeaeb1526
Yash Mathur <yashmat05@gmail.com>
2025-08-03 10:51:52 -0700

    final touches

--- scripts/train_instruct.py
+++ scripts/train_instruct.py
@@ -57,7 +57,7 @@ argParser.add_argument("--load_adapter_checkpoint_dir", type=str, default="")
 argParser.add_argument("--load_optimizer_scheduler_checkpoint_path", type=str, default="")
 
 argParser.add_argument("--torch_dtype", type=utils_argparse.str2dtype, default="bfloat16")
-argParser.add_argument("--batch_size_per_device", type=int, default=2)
+argParser.add_argument("--batch_size_per_device", type=int, default=1)
 argParser.add_argument("--num_epochs", type=int, default=24)
 argParser.add_argument("--save_every_epochs", type=int, default=1)
 argParser.add_argument("--gradient_accumulation_steps", type=int, default=32)
